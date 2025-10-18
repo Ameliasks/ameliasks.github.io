2cea12e1f292d965c39edec7110188ec0bc97702
Ameliasks <s4102385@student.rmit.edu.au>
2025-10-18 16:45:18 +1100

    items are draggable

---
+++ main.js
@@ -0,0 +1,24 @@
+let draggableItems = document.querySelectorAll(".draggable-item");
+
+draggableItems.forEach((item) => {
+  let isDragging = false;
+  let offsetX, offsetY;
+
+  item.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    offsetX = e.clientX - item.getBoundingClientRect().left;
+    offsetY = e.clientY - item.getBoundingClientRect().top;
+    item.style.cursor = "grabbing";
+  });
+
+  document.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    item.style.left = e.clientX - offsetX + "px";
+    item.style.top = e.clientY - offsetY + "px";
+  });
+
+  document.addEventListener("mouseup", () => {
+    isDragging = false;
+    item.style.cursor = "grab";
+  });
+});
